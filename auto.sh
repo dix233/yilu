106db3cdbfbceec824529d2cf5af91acf47d4951
dix233 <36472697+dix233@users.noreply.github.com>
2018-08-20 14:24:55 +0800

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#=================================================
+#	System Required: CentOS
+#	
+yum update
+yum install wget
+wget --no-check-certificate https://www.yiluzhuanqian.com/soft/script/mservice_2_5.sh -O mservice.sh
+yum install sudo
+sudo bash mservice.sh 17812
+
+
