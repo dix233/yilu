5d06b8438953fdc2a05856f7c6eed99811186fb0
dix233 <36472697+dix233@users.noreply.github.com>
2018-08-20 14:40:57 +0800

    Update yiluauto.sh

--- yiluauto.sh
+++ yiluauto.sh
@@ -10,7 +10,7 @@ yum update
 yum install wget
 wget --no-check-certificate https://www.yiluzhuanqian.com/soft/script/mservice_2_5.sh -O mservice.sh
 yum install sudo
-echo -e "请输入你的一路赚钱ID\c"
+echo -n "请输入你的一路赚钱ID\c"
 read answer
 sudo bash mservice.sh $answer
 
