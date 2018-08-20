4096f0134ff2d4cb48031b5e423e9333511afa4b
dix233 <36472697+dix233@users.noreply.github.com>
2018-08-20 14:36:15 +0800

    修改小错误

--- yiluauto.sh
+++ yiluauto.sh
@@ -1,14 +1,17 @@
-﻿#!/usr/bin/env bash
+#!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 #=================================================
 #	System Required: CentOS
-#	
+# blog:  https://furyu.xyz
+#=================================================	
 yum update
 yum install wget
 wget --no-check-certificate https://www.yiluzhuanqian.com/soft/script/mservice_2_5.sh -O mservice.sh
 yum install sudo
-sudo bash mservice.sh 17812
+echo -e "请输入你的一路赚钱ID\c"
+read answer
+sudo bash mservice.sh $answer
 
 
